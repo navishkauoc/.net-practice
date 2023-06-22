1a2ad100747576a8f7035697881f0e88dc842e67
Navishka Kularathna <navishka.randika@gmail.com>
2023-06-22 08:47:06 +0530

    Add person class and write combined name

--- 01. MSc - Enterprise Application Development/01. HelloWorld application/HelloWorld/Program.cs
+++ 01. MSc - Enterprise Application Development/01. HelloWorld application/HelloWorld/Program.cs
@@ -14,7 +14,26 @@ namespace HelloWorld
 
             String name = null;
             name = Console.ReadLine();
-            Console.Out.WriteLine("Hello " + name);
+            //Console.Out.WriteLine("Hello " + name);
+
+            Person person = new Person();
+            person.setName("Ghale");
+            Console.Out.WriteLine("Hello " + name + person.getName());
+        }
+    }
+
+    class Person
+    {
+        String name;
+
+        public String getName()
+        {
+            return name;
+        }
+
+        public void setName(String _name)
+        {
+            name = _name;
         }
     }
 }
