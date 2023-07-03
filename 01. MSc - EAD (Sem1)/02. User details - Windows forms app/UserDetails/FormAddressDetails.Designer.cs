5b21d7e704cecc9a3b0afa11f00fd6d5f44db39e
Navishka Kularathna <navishka.randika@gmail.com>
2023-07-03 15:22:50 +0530

    Add input fields and submit button

--- 01. MSc - EAD (Sem1)/02. User details - Windows forms app/UserDetails/FormAddressDetails.Designer.cs
+++ 01. MSc - EAD (Sem1)/02. User details - Windows forms app/UserDetails/FormAddressDetails.Designer.cs
@@ -29,19 +29,143 @@ namespace UserDetails
         /// </summary>
         private void InitializeComponent()
         {
+            this.labelfirstName = new System.Windows.Forms.Label();
+            this.labelLastName = new System.Windows.Forms.Label();
+            this.labelAddress = new System.Windows.Forms.Label();
+            this.labelCity = new System.Windows.Forms.Label();
+            this.labelPostCode = new System.Windows.Forms.Label();
+            this.textBoxFirstName = new System.Windows.Forms.TextBox();
+            this.textBoxSecondName = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.buttonSubmit = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // labelfirstName
+            // 
+            this.labelfirstName.AutoSize = true;
+            this.labelfirstName.Location = new System.Drawing.Point(103, 89);
+            this.labelfirstName.Name = "labelfirstName";
+            this.labelfirstName.Size = new System.Drawing.Size(57, 13);
+            this.labelfirstName.TabIndex = 0;
+            this.labelfirstName.Text = "First Name";
+            // 
+            // labelLastName
+            // 
+            this.labelLastName.AutoSize = true;
+            this.labelLastName.Location = new System.Drawing.Point(104, 116);
+            this.labelLastName.Name = "labelLastName";
+            this.labelLastName.Size = new System.Drawing.Size(75, 13);
+            this.labelLastName.TabIndex = 1;
+            this.labelLastName.Text = "Second Name";
+            // 
+            // labelAddress
+            // 
+            this.labelAddress.AutoSize = true;
+            this.labelAddress.Location = new System.Drawing.Point(104, 142);
+            this.labelAddress.Name = "labelAddress";
+            this.labelAddress.Size = new System.Drawing.Size(45, 13);
+            this.labelAddress.TabIndex = 2;
+            this.labelAddress.Text = "Address";
+            // 
+            // labelCity
+            // 
+            this.labelCity.AutoSize = true;
+            this.labelCity.Location = new System.Drawing.Point(104, 168);
+            this.labelCity.Name = "labelCity";
+            this.labelCity.Size = new System.Drawing.Size(62, 13);
+            this.labelCity.TabIndex = 3;
+            this.labelCity.Text = "City / Town";
+            // 
+            // labelPostCode
+            // 
+            this.labelPostCode.AutoSize = true;
+            this.labelPostCode.Location = new System.Drawing.Point(104, 194);
+            this.labelPostCode.Name = "labelPostCode";
+            this.labelPostCode.Size = new System.Drawing.Size(56, 13);
+            this.labelPostCode.TabIndex = 4;
+            this.labelPostCode.Text = "Post Code";
+            // 
+            // textBoxFirstName
+            // 
+            this.textBoxFirstName.Location = new System.Drawing.Point(210, 86);
+            this.textBoxFirstName.Name = "textBoxFirstName";
+            this.textBoxFirstName.Size = new System.Drawing.Size(129, 20);
+            this.textBoxFirstName.TabIndex = 5;
+            // 
+            // textBoxSecondName
+            // 
+            this.textBoxSecondName.Location = new System.Drawing.Point(210, 113);
+            this.textBoxSecondName.Name = "textBoxSecondName";
+            this.textBoxSecondName.Size = new System.Drawing.Size(129, 20);
+            this.textBoxSecondName.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(210, 139);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(129, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(210, 165);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(129, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(210, 191);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(129, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // buttonSubmit
+            // 
+            this.buttonSubmit.Location = new System.Drawing.Point(373, 277);
+            this.buttonSubmit.Name = "buttonSubmit";
+            this.buttonSubmit.Size = new System.Drawing.Size(75, 23);
+            this.buttonSubmit.TabIndex = 10;
+            this.buttonSubmit.Text = "Submit";
+            this.buttonSubmit.UseVisualStyleBackColor = true;
             // 
             // FormAddressDetails
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSubmit);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxSecondName);
+            this.Controls.Add(this.textBoxFirstName);
+            this.Controls.Add(this.labelPostCode);
+            this.Controls.Add(this.labelCity);
+            this.Controls.Add(this.labelAddress);
+            this.Controls.Add(this.labelLastName);
+            this.Controls.Add(this.labelfirstName);
             this.Name = "FormAddressDetails";
             this.Text = "Enter Your Details";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelfirstName;
+        private System.Windows.Forms.Label labelLastName;
+        private System.Windows.Forms.Label labelAddress;
+        private System.Windows.Forms.Label labelCity;
+        private System.Windows.Forms.Label labelPostCode;
+        private System.Windows.Forms.TextBox textBoxFirstName;
+        private System.Windows.Forms.TextBox textBoxSecondName;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button buttonSubmit;
     }
 }
