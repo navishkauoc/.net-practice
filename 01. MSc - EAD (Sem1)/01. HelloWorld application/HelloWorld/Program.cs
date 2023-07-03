aaf258844efa6f31293f0a52d670232e9f78a01f
Navishka Kularathna <navishka.randika@gmail.com>
2023-07-03 14:02:13 +0530

    Rename directory

--- 01. MSc - EAD (Sem1)/01. HelloWorld application/HelloWorld/Program.cs
+++ 01. MSc - EAD (Sem1)/01. HelloWorld application/HelloWorld/Program.cs
@@ -19,12 +19,27 @@ namespace HelloWorld
             Person person = new Person();
             person.setName("Ghale");
             Console.Out.WriteLine("Hello " + name + " " + person.getName());
+
+            printDetails();
+        }
+
+        private static void printDetails()
+        {
+            Person person = new Person();
+            person.setName("Aron");
+            person.Age = 30;
+            person.Address = "Arizona";
+
+            Console.Out.WriteLine(person.Age);
         }
     }
 
     class Person
     {
-        String name;
+        private String name;
+        private int age;
+        private String email;
+        private String address;
 
         public String getName()
         {
@@ -35,5 +50,12 @@ namespace HelloWorld
         {
             name = _name;
         }
+
+        public int Age { get => age; set => age = value; }
+        public string Email { get => email; set => email = value; }
+        public string Address { get => address; set => address = value; }
     }
+
+
+    
 }
