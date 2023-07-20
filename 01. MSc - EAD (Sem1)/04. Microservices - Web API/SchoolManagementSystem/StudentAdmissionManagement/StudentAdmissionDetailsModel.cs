96d1947e1fdc9e8d96d2e179e72504c8ccdd5cef
Navishka Kularathna <navishka.randika@gmail.com>
2023-07-20 17:55:39 +0530

    Add admission details model and attendance details model

---
+++ 01. MSc - EAD (Sem1)/04. Microservices - Web API/SchoolManagementSystem/StudentAdmissionManagement/StudentAdmissionDetailsModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentAdmissionManagement
+{
+    public class StudentAdmissionDetailsModel
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public string StudentClass { get; set; }
+        public DateTime DateofJoining { get; set; }
+    }
+}
---
+++ 01. MSc - EAD (Sem1)/04. Microservices - Web API/SchoolManagementSystem/StudentAttendanceManagement/StudentAttendanceDetailsModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentAttendanceManagement
+{
+    public class StudentAttendanceDetailsModel
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public double AttendencePercentage { get; set; }
+    }
+}
