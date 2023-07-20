4ec3172f116cff038f49d276387833edefb3935e
Navishka Kularathna <navishka.randika@gmail.com>
2023-07-20 21:03:43 +0530

    Add admission and attendance controllers

---
+++ 01. MSc - EAD (Sem1)/04. Microservices - Web API/SchoolManagementSystem/StudentAttendanceManagement/Controllers/StudentsAttendanceController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace StudentAttendanceManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsAttendanceController : ControllerBase
+    {
+
+        // GET: api/<StudentAttendanceController>
+        [HttpGet]
+        public IEnumerable<StudentAttendanceDetailsModel> Get()
+        {
+            StudentAttendanceDetailsModel attendanceObj1 = new StudentAttendanceDetailsModel();
+            StudentAttendanceDetailsModel attendanceObj2 = new StudentAttendanceDetailsModel();
+            attendanceObj1.StudentID = 1;
+            attendanceObj1.StudentName = "Adam";
+            attendanceObj1.AttendencePercentage = 83.02;
+            attendanceObj2.StudentID = 2;
+            attendanceObj2.StudentName = "Brad";
+            attendanceObj2.AttendencePercentage = 71.02;
+            List<StudentAttendanceDetailsModel> listObj = new List<StudentAttendanceDetailsModel>
+            {
+                attendanceObj1,
+                attendanceObj2
+            };
+            return listObj;
+        }
+
+        // GET api/<StudentsAttendanceController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<StudentsAttendanceController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<StudentsAttendanceController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<StudentsAttendanceController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
